e8ba7ef7ef8c866a34ca3c3df56522970f278f43
haseebt456 <m.haseebtufail@gmail.com>
2024-07-14 14:35:14 +0500

    Add AppPro

---
+++ AppPro.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {
+    View,
+    Text,
+    StyleSheet,//for styling
+    useColorScheme//for dark mode or light mode
+} from 'react-native'
+
+function AppPro(){
+    return(
+        <View>
+        <Text>hello world</Text>
+        </View>
+    )
+}
+
+export default AppPro;
