31aae09235f39aefbbdb8d594e6063dab88525b5
haseebt456 <m.haseebtufail@gmail.com>
2024-07-20 10:40:29 +0500

    Added dark mode in app

--- AppPro.tsx
+++ AppPro.tsx
@@ -7,12 +7,29 @@ import {
     useColorScheme//for dark mode or light mode
 } from 'react-native'
 
-function AppPro(){
+function AppPro(): JSX.Element{
+    const isDarkMode = useColorScheme() === 'dark' 
     return(
-        <View>
-        <Text>hello world</Text>
+        <View style={styles.container}>
+            <Text style = {isDarkMode ? styles.whiteText : styles.darkText}>
+                Hello World
+            </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems: 'center',//works left to right opposite to web
+        justifyContent: 'center'//works top to bottom opposite to web
+    },
+    whiteText: {
+        color: '#FFFFFF'
+    },
+    darkText: {
+        color: '#000000'
+    }
+})
 
 export default AppPro;
